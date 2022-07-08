3e7fe46aa38de346294cc90fa8fb3a413ae0c70f
Andre Garcia <andrejulio072@hotmail.com>
2022-07-08 19:03:40 +0100

    try

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "../assets/stylesheets/application.scss";
-import App from "./components/app.jsx";
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
-const root = document.getElementById("root");
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
