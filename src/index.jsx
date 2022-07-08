b77c4412a47db9802e8c4809a06ac731d9ef70b1
Andre Garcia <andrejulio072@hotmail.com>
2022-07-08 18:48:49 +0100

    remove

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import '../assets/stylesheets/application.scss';
-
+import "../assets/stylesheets/application.scss";
+import App from "./components/app.jsx";
 const Hello = ({ name }) => {
   return (
     <div>
@@ -12,7 +12,7 @@ const Hello = ({ name }) => {
   );
 };
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
